aa14f96b59be2dad93c212c9ba09b6b07f1b967d
Hans Cahya <hanscahya@outlook.com>
2021-04-10 00:14:55 +0000

    Add init commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  div
+    b-img.w-100(:src="require('@/assets/20210409_Aladin_Landing Page-02.jpg')")
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
