2e8ae0652f67b8c61b92bffce161330424e647fe
Hans Cahya <hanscahya@outlook.com>
2021-04-11 03:03:59 +0000

    Update backgroung image on mobile

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'temporary',
+    title: 'Aladin Bank',
     htmlAttrs: {
       lang: 'en'
     },
